9b8c321f8f14698a3381dc23348ea5037473054b
Charles Kasasira <charleskasasira01@gmail.com>
2022-03-04 09:31:34 +0300

    added python sample codes

---
+++ SEM-1/CSC1102 Structured-and Object-Oriented-Programming/python/interactive_shell.py
@@ -0,0 +1,9 @@
+# can run a file straight in python's interactive shell using
+# TODO: python3 -i file_name.py
+
+
+def hello_world():
+    return 'hello world!'
+
+def greetings(name):
+    return f'hello, {name}'
